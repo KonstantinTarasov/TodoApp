e36df1874a54cfc3ceb16ab59b10eabdcab22b34
SSmitt <lalaproger@yandex.ru>
2020-11-18 00:30:55 +0500

    Changed the controller and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,21 @@
 class TasksController < ApplicationController
   def index
     @task = Task.new
+    @tasks = Task.all
+  end
+
+  def create
+    @task  = Task.new(tasks_params)
+    if @task.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def tasks_params
+    params.requre(:task).permit(:ttle, :text)
   end
 end
